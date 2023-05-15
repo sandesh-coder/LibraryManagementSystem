47afaa115e815230d60f01cc9685c812dcf3825c
sandesh-coder <sandeshshinde665@gmail.com>
2023-05-15 18:28:40 +0530

    testing fixes added

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -7,7 +7,7 @@ export const sendToken = (res, student, statusCode, message) =>{
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
     }
     
-    const userData = {
+    const studentData = {
         _id: student._id,
         name: `${student.first_name +` `+ student.last_name}`,
         email: student.email,
@@ -18,5 +18,5 @@ export const sendToken = (res, student, statusCode, message) =>{
 
     res.status(statusCode)
     .cookie("token",token,options)
-    .json({ success: true,message: message, user: userData });
+    .json({ success: true,message: message, user: studentData });
 }
